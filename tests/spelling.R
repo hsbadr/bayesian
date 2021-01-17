b2cba820e6f5836187a0bd1137cf904eea47a400
Hamada S. Badr <hamada.s.badr@gmail.com>
2021-01-16 22:49:40 -0500

    tests: Skip spelling check on CI & GHA

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,4 +1,6 @@
-if (requireNamespace("spelling", quietly = TRUE)) {
+if (requireNamespace("spelling", quietly = TRUE) &&
+  !nzchar(Sys.getenv("CI")) &&
+  !nzchar(Sys.getenv("GITHUB_ACTIONS"))) {
   spelling::spell_check_test(
     vignettes = TRUE, error = FALSE,
     skip_on_cran = TRUE
