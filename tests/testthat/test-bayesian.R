c60725d051009d7ee02c5f5af4bab197a01ab89b
Hamada S. Badr <hamada.s.badr@gmail.com>
2021-05-18 15:34:31 -0400

    tests: Update Bayesian model specifciation

--- tests/testthat/test-bayesian.R
+++ tests/testthat/test-bayesian.R
@@ -25,8 +25,12 @@ test_that("bayesian execution", {
       set.seed(2020)
       expect_error(
         bayesian_mod <-
-          bayesian() %>%
-          set_engine("brms", iter = 500, seed = 2020, refresh = 0) %>%
+          bayesian(
+            iter = 500,
+            seed = 2020,
+            stan_args = list(refresh = 0)
+          ) %>%
+          set_engine("brms") %>%
           fit(
             rating ~ treat + period + carry + (1 | subject),
             data = inhaler
