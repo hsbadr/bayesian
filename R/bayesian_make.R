5d60f9a58bb041b385be3797ce1730ad64d5b61b
Hamada S. Badr <hamada.s.badr@gmail.com>
2022-01-07 13:50:01 -0500

    `bayesian`: Use `rlang::fn_fmls_names()`

--- R/bayesian_make.R
+++ R/bayesian_make.R
@@ -28,7 +28,7 @@ bayesian_make <- function(modes = c("classification", "regression")) {
     # -------------------------------------------------------------------------
 
     for (pkg in dependpkgs) {
-      if ("mode" %in% names(formals(parsnip::set_dependency))) {
+      if ("mode" %in% rlang::fn_fmls_names(parsnip::set_dependency)) {
         parsnip::set_dependency(model, engine, pkg = pkg, mode = mode)
       } else {
         parsnip::set_dependency(model, engine, pkg = pkg)
